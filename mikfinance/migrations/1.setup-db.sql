c0b247c3f49d05b4826ed70c9e2f6217d8c22ea3
Mikael <mikaelel902@gmail.com>
2026-08-06 13:24:15 +0700

    data structure & vector function part 1

---
+++ mikfinance/migrations/1.setup-db.sql
@@ -0,0 +1,25 @@
+-- setup pgvector extension
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Create table transactions
+CREATE TABLE IF NOT EXISTS public.transactions(
+    id UUID DEFAULT get_random_uuid(),
+    type TEXT NOT NULL CHECK (type IN ('income', 'expense')),
+    category TEXT NOT NULL,
+    amount NUMERIC NOT NULL,
+    description TEXT,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    user_id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
+    embedding VECTOR(768),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('utc'::text, now()) NOT NULL
+);
+
+-- Aktivasi RLS untuk semua diperbolehkan
+ALTER TABLE public.transactions ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Permissive rules for all" ON public.transactions
+    FOR ALL USING (true);
+
+--jika sudah ada sistem auth
+-- CREATE POLICY "Users can manage their own transactions" ON public.transactions
+--     FOR ALL USING (auth.uid() = user_id);

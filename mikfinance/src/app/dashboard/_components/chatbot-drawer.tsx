c4a44362a003b40c6632bd5d0080e7fb5d352562
Mikael <mikaelel902@gmail.com>
2026-08-14 12:16:28 +0700

    buat UI AI Chatbot par 2

--- mikfinance/src/app/dashboard/_components/chatbot-drawer.tsx
+++ mikfinance/src/app/dashboard/_components/chatbot-drawer.tsx
@@ -12,8 +12,36 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function ChatbotDrawer() {
+  const [conversation, setConversation] = useState<
+    {
+      role: string;
+      parts: {
+        text: string;
+      }[];
+    }[]
+  >([
+    {
+      role: "user",
+      parts: [
+        {
+          text: "Hello",
+        },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
+        {
+          text: "Hello, how can i help you?",
+        },
+      ],
+    },
+  ]);
+
   return (
     <Drawer direction="right" modal={false}>
       <DrawerTrigger className="fixed bottom-4 right-4" asChild>
@@ -44,6 +72,35 @@ export default function ChatbotDrawer() {
             </Button>
           </DrawerClose>
         </DrawerHeader>
+        <div className="no-scrollbar overflow-y-auto px-4 h-full">
+          {conversation.length > 0 ? (
+            <div className="flex-col h-full overflow-x-hidden no-scrollbar overflow-y-auto gap-8">
+              {conversation.map((message, index) => (
+                <div
+                  key={`conversation-${index}`}
+                  className={cn(
+                    "flex flex-col gap-2",
+                    message.role === "model" ? "items-start" : "items-end",
+                  )}
+                >
+                  <div
+                    className={cn("flex flex-col w-full", {
+                      "bg-primary/20 text-primary px-5 py-2 rounded-3xl rounted-br-md w-fit max-w-3/4":
+                        message.role === "user",
+                    })}
+                  >
+                    {message.parts[0].text}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full">
+              <h2 className="text-3xl font-bold text-primary">Hello Fuck!!!</h2>
+              <h4 className="text-xl">What can i help you Motherfucker?</h4>
+            </div>
+          )}
+        </div>
       </DrawerContent>
     </Drawer>
   );

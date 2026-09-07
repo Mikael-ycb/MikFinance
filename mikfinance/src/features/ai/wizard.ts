e1198d1502ea593b6a2255ac3ec940cf1e426405
Mikael <mikaelel902@gmail.com>
2026-09-07 10:57:39 +0700

    Compositional Function Calling (autonomus) part 2

--- mikfinance/src/features/ai/wizard.ts
+++ mikfinance/src/features/ai/wizard.ts
@@ -2,13 +2,19 @@
 
 import z from "zod";
 import { createAI } from "./instance";
-import { FunctionDeclaration, Type } from "@google/genai";
+import { Content } from "@google/genai";
 import {
   createTransaction,
   deleteTransaction,
   updateTransaction,
 } from "../transaction/action";
 import { findEmbedding } from "./embedding";
+import {
+  createTransactionDeclaration,
+  deleteTransactionDeclaration,
+  getTransactionDeclaration,
+  updateTransactionDeclaration,
+} from "./functionTransaction";
 
 const transactionSchema = z.object({
   amount: z.number().default(0).describe("Transaction nominal"),
@@ -49,7 +55,7 @@ export async function handleWizardInput(message: string) {
   `;
   const ai = createAI();
   const response = await ai.models.generateContent({
-    model: "gemini-3.7-flash",
+    model: "gemini-3.5-flash",
     contents,
     config: {
       responseMimeType: "application/json",
@@ -66,146 +72,120 @@ export async function handleWizardInput(message: string) {
   return "Create transaction success";
 }
 
-const transactionProperties = {
-  id: {
-    type: Type.STRING,
-    description: "The unique identifier of the Transaction",
-  },
-  amount: {
-    type: Type.NUMBER,
-    description: "The amounth of the transaction",
-  },
-  type: {
-    type: Type.STRING,
-    enum: ["income", "expense"],
-    description: "The type the transaction, either  'income' or 'expense'",
-  },
-  category: {
-    type: Type.STRING,
-    enum: ["Food & Drink", "Transport", "Reword", "Salary", "Invest", "Others"],
-    description: "The Category of the transaction",
-  },
-  description: {
-    type: Type.STRING,
-    description:
-      "A brief description of the transaction, first letter capitalized",
-  },
-  date: {
-    type: Type.STRING,
-    description: 'The date of the transaction in the format "YYYY-MM-DD"',
-  },
-};
-
-const createTransactionDeclaration: FunctionDeclaration = {
-  name: "create_transaction",
-  description:
-    "Create a new transaction in the user's financial history based on the provided details.",
-  parameters: {
-    type: Type.OBJECT,
-    properties: transactionProperties,
-    required: ["amount", "description", "type", "category", "date"],
-  },
-};
-
-const deleteTransactionDeclaration: FunctionDeclaration = {
-  name: "delete_transaction",
-  description:
-    "Delete an existing transaction from user's financial history based on the provided data.",
-  parameters: {
-    type: Type.OBJECT,
-    properties: transactionProperties,
-  },
-};
-
-const updateTransactionDeclaration: FunctionDeclaration = {
-  name: "update_transaction",
-  description:
-    "update an existing transaction from user's financial history based on the provided data.",
-  parameters: {
-    type: Type.OBJECT,
-    properties: transactionProperties,
-  },
-};
-
 export async function handleWizardTools(message: string) {
-  const contents = `
+  const contents: Content[] = [
+    {
+      role: "user",
+      parts: [
+        {
+          text: `
   <role>
     You are an AI Wizard finance assistant, who can extract transaction details from text.
   </role>
   <instruction>
   Extract the transaction detail from the following text.
+  If request is to update or delete transaction, you must call function get_treansaction first to find out which transaction will be updated or deleted. 
   </instruction>
   <context>
     Current Date:  ${new Date().toISOString()}
   </context>
   <input>
       Text to extract: ${message}
-  </input>`;
-
-  const ai = createAI();
-  const response = await ai.models.generateContent({
-    model: "gemini-3.5-flash",
-    contents,
-    config: {
-      tools: [
-        {
-          functionDeclarations: [
-            createTransactionDeclaration,
-            deleteTransactionDeclaration,
-            updateTransactionDeclaration,
-          ],
+  </input>`,
         },
       ],
     },
-  });
+  ];
 
-  if (response.functionCalls && response.functionCalls.length > 0) {
-    await Promise.all(
-      response.functionCalls.map(async (functionCall) => {
-        const args = functionCall.args;
-        if (!args) {
-          throw new Error("No arguments provided for Action");
-        }
-        switch (functionCall.name) {
-          case "create_transaction":
-            const transaction = transactionSchema.parse(args);
-            if (transaction.amount <= 0) {
-              throw new Error("Cannot create transaction with invalid amount");
-            }
-            await createTransaction(transaction);
-            break;
-          case "delete_transaction":
-            const dataFindForDelete = await findEmbedding(
-              JSON.stringify(args),
-              0.3,
-              1,
-            );
-            const deletedData = dataFindForDelete[0];
-            await deleteTransaction(deletedData.id);
-            break;
-          case "update_transaction":
-            const dataFindForUpdate = await findEmbedding(
-              JSON.stringify(args),
-              0.3,
-              1,
-            );
-            const updateData = dataFindForUpdate[0];
+  const ai = createAI();
+  let running = true;
+  while (running) {
+    const response = await ai.models.generateContent({
+      model: "gemini-3.7-flash",
+      contents,
+      config: {
+        tools: [
+          {
+            functionDeclarations: [
+              getTransactionDeclaration,
+              createTransactionDeclaration,
+              deleteTransactionDeclaration,
+              updateTransactionDeclaration,
+            ],
+          },
+        ],
+      },
+    });
 
-            const newData = transactionSchema.parse(args);
-            if (newData.amount <= 0) {
-              throw new Error("Cannot update transaction with invalid amount");
-            }
+    if (response.functionCalls && response.functionCalls.length > 0) {
+      if (response.candidates && response.candidates[0]?.content) {
+        contents.push(response.candidates[0].content);
+      }
 
-            await updateTransaction(updateData.id, newData);
-            break;
-          default:
-            throw new Error("Unknow function call");
-        }
-      }),
-    );
+      const functionResponseParts = await Promise.all(
+        response.functionCalls.map(async (functionCall) => {
+          const { name, args, id } = functionCall;
+          if (!args) {
+            throw new Error("No arguments provided for Action");
+          }
 
-    return "Function executed successfully";
-  } else {
-    throw new Error("AI did not call any function");
+          let resultData = {};
+
+          switch (name) {
+            case "get_transaction":
+              const dataFind = await findEmbedding(
+                JSON.stringify(args),
+                0.3,
+                1,
+              );
+              resultData = dataFind[0] || {};
+              break;
+
+            case "create_transaction":
+              const transaction = transactionSchema.parse(args);
+              if (transaction.amount <= 0) {
+                throw new Error(
+                  "Cannot create transaction with invalid amount",
+                );
+              }
+              await createTransaction(transaction);
+              break;
+
+            case "delete_transaction":
+              await deleteTransaction(`${args.id}`);
+              break;
+
+            case "update_transaction":
+              const newData = transactionSchema.parse(args);
+              if (newData.amount <= 0) {
+                throw new Error(
+                  "Cannot update transaction with invalid amount",
+                );
+              }
+
+              await updateTransaction(`${args.id}`, newData);
+              break;
+            default:
+              throw new Error("Unknow function call");
+          }
+
+          return {
+            functionResponse: {
+              name,
+              response: { result: resultData },
+              id,
+            },
+          };
+        }),
+      );
+
+      contents.push({
+        role: "user",
+        parts: functionResponseParts,
+      });
+    } else {
+      running = false;
+      return response.text;
+    }
   }
 }
